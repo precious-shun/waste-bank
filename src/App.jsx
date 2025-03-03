f812377d14fa3c64ee34a2e66025837db346c838
Sultan Viqor KRS <sultanviqor@gmail.com>
2025-03-03 12:20:44 +0700

    [ADD] tailwindcss and mui

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className="flex flex-col">
+        <Button variant="text" color="error">
+          Text
+        </Button>
+        <Button variant="contained">Contained</Button>
+        <Button variant="outlined">Outlined</Button>
+        <p className="text-red-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
+          consequatur aliquam quod ab in, voluptates possimus minus libero
+          labore ex ducimus excepturi error perspiciatis! Vero eos dicta aperiam
+          modi unde?
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
