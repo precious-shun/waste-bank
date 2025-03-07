d203d6ff35c8c9e1402b717e3beb3063b1c09874
Arief Kamaluddin <ariefkamaluddin.ak96@gmail.com>
2025-03-07 10:48:02 +0700

    [MODIFY] Modify code in modal selectedTransaction

--- src/pages/TransactionHistory.jsx
+++ src/pages/TransactionHistory.jsx
@@ -146,7 +146,7 @@ function TransactionHistory() {
 
       {selectedTransaction && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded-2xl">
+          <div className="bg-white p-6 rounded-2xl w-96">
             <h3 className="text-lg font-semibold mb-4">Detail Transaksi</h3>
             <ul className="mb-4">
               {selectedTransaction.waste_products.map((product, idx) => (
