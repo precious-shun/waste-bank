959d42fe4cc49f9f9fe9eb480f644b49eac0340d
Sunan Alit <sunanalit@gmail.com>
2025-03-08 13:19:52 +0700

    [ADD] user profile now under auth system

--- src/pages/user/UserProfile.jsx
+++ src/pages/user/UserProfile.jsx
@@ -1,8 +1,34 @@
 import { Avatar, Button, Divider, TextField } from "@mui/material";
 import Navbar from "../../components/Navbar";
 import { theme } from "../../theme";
+import { useAuth } from "../../context/AuthContext";
+import { useState, useEffect } from "react";
+import { db } from "../../firebase";
+import { doc, getDoc } from "firebase/firestore";
 
 const UserProfile = () => {
+  const { user } = useAuth();
+  const [profile, setProfile] = useState({
+    fullname: "",
+    address: "",
+    email: "",
+    gender: "",
+  });
+
+  useEffect(() => {
+    const fetchUserProfile = async () => {
+      if (!user || !user.uid) return; // Ensure user is logged in
+      const userRef = doc(db, "users", user.uid);
+      const userSnap = await getDoc(userRef);
+
+      if (userSnap.exists()) {
+        setProfile(userSnap.data()); // Set state with user data
+      }
+    };
+
+    fetchUserProfile();
+  }, [user]);
+
   return (
     <>
       <Navbar />
@@ -12,29 +38,53 @@ const UserProfile = () => {
           <div className="border border-1 mt-6 p-6 rounded-lg gap-4 flex flex-col">
             <div className="flex items-center">
               <span className="w-1/6">Profile Picture</span>
-              <Avatar sx={{ width: 48, height: 48 }}></Avatar>
+              <Avatar sx={{ width: 48, height: 48 }} />
             </div>
-            <Divider></Divider>
+
+            <Divider />
             <div className="flex items-center">
               <span className="w-1/5">Name</span>
-              <TextField fullWidth value={"Arif Camaludin"} size="small"></TextField>
+              <TextField
+                fullWidth
+                size="small"
+                defaultValue={profile?.fullname || ""}
+              />
             </div>
-            <Divider></Divider>
+            <Divider />
             <div className="flex items-center">
               <span className="w-1/5">Address</span>
-              <TextField fullWidth value={"Bogor"} size="small"></TextField>
+              <TextField
+                fullWidth
+                size="small"
+                defaultValue={profile?.address || ""}
+              />
             </div>
-            <Divider></Divider>
+            <Divider />
             <div className="flex items-center">
               <span className="w-1/5">Email</span>
-              <TextField fullWidth value={"arif@gmail.com"} size="small"></TextField>
+              <TextField
+                fullWidth
+                size="small"
+                defaultValue={profile?.email || ""}
+              />
             </div>
-            <Divider></Divider>
+            <Divider />
             <div className="flex items-center mb-1.5">
               <span className="w-1/5">Gender</span>
-              <TextField fullWidth value={"Male"} size="small"></TextField>
+              <TextField
+                fullWidth
+                size="small"
+                defaultValue={profile?.gender || ""}
+              />
             </div>
-            <Button variant="contained" sx={{ marginLeft: "auto", textTransform: "none", backgroundColor: theme.green }}>
+            <Button
+              variant="contained"
+              sx={{
+                marginLeft: "auto",
+                textTransform: "none",
+                backgroundColor: theme.green,
+              }}
+            >
               Update Profile
             </Button>
           </div>
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,10 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-<<<<<<< HEAD
 import { getAnalytics } from "firebase/analytics";
-=======
-// import { getAnalytics } from "firebase/analytics";
->>>>>>> waste-prices
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
@@ -24,11 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-<<<<<<< HEAD
 const analytics = getAnalytics(app);
-=======
-// const analytics = getAnalytics(app);
->>>>>>> waste-prices
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
@@ -52,8 +44,4 @@ export const db = getFirestore(app);
 // const app = initializeApp(firebaseConfig);
 
 // export const auth = getAuth(app);
-<<<<<<< HEAD
 // export const db = getFirestore(app);
-=======
-// export const db = getFirestore(app);
->>>>>>> waste-prices
