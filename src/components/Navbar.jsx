6c58797247d6e614cb48274a04570f72c8688605
Bagas Ariantono <bagasariantono@gmail.com>
2025-03-10 00:28:47 +0700

    [MODIFY] Responsive in navbar and user profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,9 @@ import Logo from "../assets/logo.svg";
 import { useAuth } from "../context/AuthContext";
 import { useLocation, useNavigate } from "react-router-dom";
 import NotificationBell from "./NotificationBell";
+import { theme } from "../theme";
+import { Bars3Icon } from '@heroicons/react/24/solid'
+import { useState } from "react";
 
 const colors = {
   darkGreen: "#2C514B",
@@ -13,6 +16,13 @@ const colors = {
   white: "#ffffff",
 };
 
+const navList = [
+  { label: "Home", path: "/user/" },
+  { label: "Profile", path: "/user/profile" },
+  { label: "Detailed History", path: "/user/history" },
+  { label: "Waste Exchange Rate", path: "/user/calc" },
+]
+
 const Navbar = () => {
   const { logout } = useAuth();
 
@@ -22,7 +32,11 @@ const Navbar = () => {
     logout();
   };
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  console.log(isMenuOpen)
+
   return (
+    <>
     <AppBar
       position="static"
       sx={{
@@ -42,61 +56,72 @@ const Navbar = () => {
         {/* Left Side: Logo & Tabs */}
         <Box sx={{ display: "flex", alignItems: "center", gap: 3 }}>
           <Box component="img" src={Logo} alt="Logo" sx={{ height: 65 }} />
-          <Box sx={{ display: "flex", gap: 1 }}>
-            {[
-              { label: "Home", path: "/user/" },
-              { label: "Profile", path: "/user/profile" },
-              { label: "Detailed History", path: "/user/history" },
-              { label: "Waste Exchange Rate", path: "/user/calc" },
-            ].map(({ label, path }) => (
-              <Button
-                key={label}
-                onClick={() => navigate(path)}
-                sx={{
-                  backgroundColor:
-                    location.pathname === path
-                      ? colors.orange
-                      : colors.darkGreen,
-                  color: colors.white,
-                  borderRadius: "8px 8px 0 0", // Tab-like shape
-                  padding: "8px 16px",
-                  fontWeight: "bold",
-                  borderBottom:
-                    location.pathname === path
-                      ? "none"
-                      : `2px solid ${colors.lightGreen}`,
-                  "&:hover": {
-                    backgroundColor: colors.green,
-                  },
-                }}
-              >
-                {label}
-              </Button>
-            ))}
-          </Box>
+          <div className="hidden sm:flex">
+            <Box sx={{ display: "flex", gap: 1 }}>
+              {navList.map(({ label, path }) => (
+                <Button
+                  key={label}
+                  onClick={() => navigate(path)}
+                  sx={{
+                    backgroundColor:
+                      location.pathname === path
+                        ? colors.orange
+                        : colors.darkGreen,
+                    color: colors.white,
+                    borderRadius: "8px 8px 0 0", // Tab-like shape
+                    padding: "8px 16px",
+                    fontWeight: "bold",
+                    borderBottom:
+                      location.pathname === path
+                        ? "none"
+                        : `2px solid ${colors.lightGreen}`,
+                    "&:hover": {
+                      backgroundColor: colors.green,
+                    },
+                  }}
+                >
+                  {label}
+                </Button>
+              ))}
+            </Box>
+          </div>
         </Box>
 
         {/* Right Side: Logout Button */}
-        <div className="space-x-4">
+        <div className="space-x-4 flex">
           <NotificationBell />
-          <Button
-            onClick={handleLogout}
-            sx={{
-              backgroundColor: colors.orange,
-              color: colors.white,
-              borderRadius: "20px",
-              padding: "6px 16px",
-              fontWeight: "bold",
-              "&:hover": {
-                backgroundColor: colors.green,
-              },
-            }}
-          >
-            Logout
-          </Button>
+          <div className="hidden sm:flex">
+            <Button
+              onClick={handleLogout}
+              sx={{
+                backgroundColor: colors.orange,
+                color: colors.white,
+                borderRadius: "20px",
+                padding: "6px 16px",
+                fontWeight: "bold",
+                "&:hover": {
+                  backgroundColor: colors.green,
+                },
+              }}
+            >
+              Logout
+            </Button>
+          </div>
+          <button onClick={() => setIsMenuOpen((prev) => !prev)} className="sm:hidden"><Bars3Icon className="text-white size-6"/></button>
         </div>
       </Toolbar>
     </AppBar>
+    <div className={`${isMenuOpen ? "" : "hidden"} bg-darkgreen border flex border-t-1 flex-col gap-3 border-normalgreen z-10 p-5 top-20`}>
+      {navList.map(({label, path}) => (
+      <>
+        <button onClick={() => navigate(path)} style={{backgroundColor: location.pathname === path ? `${theme.orange}` : `${theme.darkGreen}` , borderBottom: location.pathname === path ? "" : "1.2px solid white", color: "white", fontWeight: "bold"}} className="p-2.5 text-left rounded-t-lg">
+          {label.toUpperCase()}
+        </button>
+      </>
+      ))
+      }
+    </div>
+    </>
   );
 };
 
