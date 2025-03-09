653fc09639bcd2501852adc159ef6b42f33ab1b8
Sunan Alit <sunanalit@gmail.com>
2025-03-09 11:11:25 +0700

    [ADD, MODIFY] adding button to login and register and modify style

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -12,8 +12,10 @@ import {
   Card,
   CardContent,
   Typography,
+  Divider,
 } from "@mui/material";
 import { theme } from "../theme";
+import Logo from "../assets/logo.svg";
 
 const loginSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -26,6 +28,12 @@ const inputStyle = {
   },
   ":hover .MuiInputLabel-outlined ": {
     color: theme.darkGreen,
+  },
+  ".MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: theme.orange,
+  },
+  ".MuiInputLabel-root.Mui-focused": {
+    color: theme.orange,
   },
 };
 
@@ -69,64 +77,96 @@ const Login = () => {
 
   return (
     <div
-      style={{ backgroundColor: theme.lightGrey }}
+      style={{ backgroundColor: theme.darkGreen }}
       className="flex justify-center items-center h-screen"
     >
-      <Card
-        variant="outlined"
-        style={{ backgroundColor: theme.lightGreen }}
-        sx={{ borderRadius: 4 }}
-        className="w-96 px-1.5 py-2"
-      >
-        <CardContent>
-          <p
-            style={{ color: theme.darkGreen }}
-            className="text-center text-2xl font-semibold mb-6"
+      <div className="flex w-4/5 items-center justify-center space-x-10">
+        <div className="w-1/2 flex justify-center">
+          <img src={Logo} alt="Logo" className="w-144" />
+        </div>
+        <div className="w-1/3">
+          <Card
+            variant="outlined"
+            style={{ backgroundColor: theme.lightGrey }}
+            sx={{ borderRadius: 4 }}
+            className="w-96 px-1.5 py-2"
           >
-            Login
-          </p>
-          <form onSubmit={handleSubmit(handleLogin)} className="space-y-6">
-            <div className="space-y-4">
-              <TextField
-                sx={inputStyle}
-                fullWidth
-                label="Email"
-                variant="outlined"
-                {...register("email")}
-                error={!!errors.email}
-                helperText={errors.email?.message}
-              />
-
-              <TextField
-                sx={inputStyle}
-                fullWidth
-                label="Password"
-                type="password"
-                variant="outlined"
-                {...register("password")}
-                error={!!errors.password}
-                helperText={errors.password?.message}
-              />
-
-              {error && (
-                <Typography color="error" className="text-sm">
-                  {error}
+            <CardContent>
+              <p
+                style={{ color: theme.darkGreen }}
+                className="text-center text-2xl font-semibold mb-6"
+              >
+                Login
+                <Typography
+                  className="text-center mb-6"
+                  style={{ color: theme.darkGreen }}
+                >
+                  Input your email and password
                 </Typography>
-              )}
-            </div>
+              </p>
+              <form onSubmit={handleSubmit(handleLogin)} className="space-y-6">
+                <div className="space-y-4">
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    {...register("email")}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                  />
 
-            <Button
-              size="large"
-              sx={{ backgroundColor: theme.green }}
-              type="submit"
-              variant="contained"
-              fullWidth
-            >
-              Login
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    {...register("password")}
+                    error={!!errors.password}
+                    helperText={errors.password?.message}
+                  />
+
+                  {error && (
+                    <Typography color="error" className="text-sm">
+                      {error}
+                    </Typography>
+                  )}
+                </div>
+
+                <Button
+                  size="large"
+                  sx={{ backgroundColor: theme.green }}
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                >
+                  Login
+                </Button>
+              </form>
+
+              <Divider sx={{ my: 3 }} />
+
+              <Typography
+                className="text-center"
+                style={{ color: theme.darkGreen }}
+              >
+                Don't have an account yet?
+              </Typography>
+
+              <Button
+                size="large"
+                sx={{ backgroundColor: theme.orange, mt: 2 }}
+                variant="contained"
+                fullWidth
+                onClick={() => navigate("/register")}
+              >
+                Register
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,8 +4,18 @@ import { z } from "zod";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth, db } from "../services/firebase";
 import { doc, setDoc } from "firebase/firestore";
-import { TextField, Button, Card, CardContent, MenuItem } from "@mui/material";
+import {
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  MenuItem,
+  Typography,
+  Divider,
+} from "@mui/material";
 import { theme } from "../theme";
+import { useNavigate } from "react-router-dom";
+import Logo from "../assets/logo.svg";
 
 const registerSchema = z.object({
   email: z.string().email("Invalid email address"),
@@ -22,6 +32,12 @@ const inputStyle = {
   ":hover .MuiInputLabel-outlined ": {
     color: theme.darkGreen,
   },
+  ".MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline": {
+    borderColor: theme.orange,
+  },
+  ".MuiInputLabel-root.Mui-focused": {
+    color: theme.orange,
+  },
 };
 
 const Register = () => {
@@ -33,9 +49,15 @@ const Register = () => {
     resolver: zodResolver(registerSchema),
   });
 
+  const navigate = useNavigate();
+
   const handleRegister = async (data) => {
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, data.email, data.password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
       const user = userCredential.user;
 
       await setDoc(doc(db, "users", user.uid), {
@@ -54,33 +76,123 @@ const Register = () => {
   };
 
   return (
-    <div style={{ backgroundColor: theme.lightGrey }} className="flex justify-center items-center h-screen">
-      <Card variant="outlined" sx={{ borderRadius: 4 }} style={{ backgroundColor: theme.lightGreen }} className="w-96 px-1.5">
-        <CardContent>
-          <p style={{ color: theme.darkGreen }} className="text-center text-2xl font-semibold mb-6">
-            Register
-          </p>
-          <form onSubmit={handleSubmit(handleRegister)} className="space-y-6">
-            <div className="space-y-4">
-              <TextField sx={inputStyle} fullWidth label="Email" variant="outlined" {...register("email")} error={!!errors.email} helperText={errors.email?.message} />
+    <div
+      style={{ backgroundColor: theme.darkGreen }}
+      className="flex justify-center items-center h-screen"
+    >
+      <div className="flex w-4/5 items-center justify-center space-x-10">
+        <div className="w-1/2 flex justify-center">
+          <img src={Logo} alt="Logo" className="w-144" />
+        </div>
+        <div className="w-1/3">
+          <Card
+            variant="outlined"
+            sx={{ borderRadius: 4 }}
+            style={{ backgroundColor: theme.lightGrey }}
+            className="w-96 px-1.5"
+          >
+            <CardContent>
+              <p
+                style={{ color: theme.darkGreen }}
+                className="text-center text-2xl font-semibold mb-6"
+              >
+                Register
+                <Typography
+                  className="text-center mb-6"
+                  style={{ color: theme.darkGreen }}
+                >
+                  Please fill in your data
+                </Typography>
+              </p>
+              <form
+                onSubmit={handleSubmit(handleRegister)}
+                className="space-y-6"
+              >
+                <div className="grid grid-cols-2 gap-4">
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    label="Email"
+                    variant="outlined"
+                    {...register("email")}
+                    error={!!errors.email}
+                    helperText={errors.email?.message}
+                  />
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    label="Password"
+                    type="password"
+                    variant="outlined"
+                    {...register("password")}
+                    error={!!errors.password}
+                    helperText={errors.password?.message}
+                  />
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    label="Full Name"
+                    variant="outlined"
+                    {...register("fullname")}
+                    error={!!errors.fullname}
+                    helperText={errors.fullname?.message}
+                  />
+                  <TextField
+                    sx={inputStyle}
+                    fullWidth
+                    select
+                    label="Gender"
+                    variant="outlined"
+                    {...register("gender")}
+                    error={!!errors.gender}
+                    helperText={errors.gender?.message}
+                  >
+                    <MenuItem value="Male">Male</MenuItem>
+                    <MenuItem value="Female">Female</MenuItem>
+                  </TextField>
+                </div>
+                <TextField
+                  sx={inputStyle}
+                  fullWidth
+                  label="Address"
+                  variant="outlined"
+                  {...register("address")}
+                  error={!!errors.address}
+                  helperText={errors.address?.message}
+                />
+                <Button
+                  size="large"
+                  sx={{ backgroundColor: theme.green }}
+                  type="submit"
+                  variant="contained"
+                  fullWidth
+                >
+                  Register
+                </Button>
+              </form>
 
-              <TextField sx={inputStyle} fullWidth label="Password" type="password" variant="outlined" {...register("password")} error={!!errors.password} helperText={errors.password?.message} />
+              <Divider sx={{ my: 3 }} />
 
-              <TextField sx={inputStyle} fullWidth label="Full Name" variant="outlined" {...register("fullname")} error={!!errors.fullname} helperText={errors.fullname?.message} />
+              <Typography
+                className="text-center"
+                style={{ color: theme.darkGreen }}
+              >
+                Already have an account?
+              </Typography>
 
-              <TextField value={""} sx={inputStyle} fullWidth select label="Gender" variant="outlined" {...register("gender")} error={!!errors.gender} helperText={errors.gender?.message}>
-                <MenuItem value="Male">Male</MenuItem>
-                <MenuItem value="Female">Female</MenuItem>
-              </TextField>
-
-              <TextField sx={inputStyle} fullWidth label="Address" variant="outlined" {...register("address")} error={!!errors.address} helperText={errors.address?.message} />
-            </div>
-            <Button size="large" sx={{ backgroundColor: theme.green }} type="submit" variant="contained" fullWidth>
-              Register
-            </Button>
-          </form>
-        </CardContent>
-      </Card>
+              <Button
+                size="large"
+                sx={{ backgroundColor: theme.orange, mt: 2 }}
+                variant="contained"
+                fullWidth
+                onClick={() => navigate("/")}
+              >
+                Login
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
