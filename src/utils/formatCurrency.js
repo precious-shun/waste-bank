42b38b4766380c68247fd4845aeb7cd295739e50
Sultan Viqor KRS <sultanviqor@gmail.com>
2025-03-10 08:29:13 +0700

    [MODIFY] remove decimal

--- src/utils/formatCurrency.js
+++ src/utils/formatCurrency.js
@@ -2,5 +2,7 @@ export const formatCurrency = (amount) => {
   return new Intl.NumberFormat("id-ID", {
     style: "currency",
     currency: "IDR",
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 0,
   }).format(amount);
 };
